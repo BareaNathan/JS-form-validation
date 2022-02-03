76c28e29477fee3251606577e8977942a36c3369
Nathan Barea <nathanszekutb@hotmail.com>
2022-02-03 12:01:24 -0300

    Aula 3

--- validacao-doguito-main/js/validacao.js
+++ validacao-doguito-main/js/validacao.js
@@ -30,12 +30,17 @@ const mensagensDeErro = {
     dataNascimento:{
         valueMissing: 'O campo de data de Nascimento não pode estar vazio.',
         customError: 'Você deve ser maior de 18 anos para se cadastrar.'
+    },
+    cpf: {
+        valueMissing: 'O campo CPF não pode estar vazio.',
+        customError: 'O CPF digitado não é valido.'
     }
 
 }
 
 const validadores = {
-    dataNascimento:input => validaDataNascimento(input)
+    dataNascimento:input => validaDataNascimento(input),
+    cpf:input => validaCPF(input)
 }
 
 const tiposDeErro = [
@@ -71,4 +76,70 @@ function maiorQue18(data){
     const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
 
     return dataMais18 <= dataAtual
+}
+
+function validaCPF(input){
+    const cpfFormatado = input.value.replace(/\D/g, '')
+    let mensagem = ''
+
+    if(!checaCPFRepetido(cpfFormatado) || !checaEstruturaCPF(cpfFormatado)){
+        mensagem = 'O CPF digitado não é valido.'
+    }
+
+    input.setCustomValidity(mensagem);
+}
+
+function checaCPFRepetido(cpf){
+    const valoresRepetidos=[
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ]
+    let cpfValido = true
+
+    valoresRepetidos.forEach(valor => {
+        if(valor == cpf){
+            cpfValido = false
+        }
+    })
+
+    return cpfValido
+}
+
+function checaEstruturaCPF(cpf){
+    const multiplicador = 10
+
+    return checaDigitoVerificador(cpf,multiplicador)
+}
+
+function checaDigitoVerificador(cpf, multiplicador){
+    if(multiplicador >= 12){
+        return true
+    }
+
+    let multiplicadorInicial = multiplicador
+    let soma = 0
+    const cpfSemDigitos = cpf.substring(0,multiplicador - 1).split('')
+    const digitoVerificador = cpf.charAt(multiplicador - 1)
+    for(let contador = 0; multiplicadorInicial > 1; multiplicadorInicial--){
+        soma = soma + multiplicadorInicial*cpfSemDigitos[contador]
+        contador++
+    }
+
+    if (digitoVerificador == confirmaDigito(soma)){
+        return checaDigitoVerificador(cpf, multiplicador + 1)
+    }
+
+    return false
+}
+
+function confirmaDigito(soma){
+    return 11 - (soma % 11)
 }
